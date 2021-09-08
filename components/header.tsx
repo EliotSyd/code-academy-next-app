24d4b9fa3f6be72d885b7dcc537f99fffa1b9d40
EliotSyd <eliot.sydenham@gmail.com>
2021-09-08 17:24:56 +0000

    gradient change

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 const Header: React.FC = () => {
     return (
-<header  className="h-16 px-8 bg-blue-600 text-white w-full sticky top-0 flex items-center justify-between">
+<header  className="h-16 px-8 bg-gradient-to-r from-blue-700 to-blue-100  text-white w-full sticky top-0 flex items-center justify-between">
       HEADER
 	<span>
     <div className="h-10 w-10 bg-white rounded-full flex items-center justify-center text-blue-600">
