0d1a3c1a35029defb9cc7d081b73a26cb02c7fd5
EliotSyd <eliot.sydenham@gmail.com>
2021-09-15 13:57:40 +0100

    Changed

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const Footer: React.FC = () => {
-    return (
-        <footer className="
+  return (
+    <footer className="
         w-full 
         h-24 
         flex 
@@ -10,23 +10,23 @@ const Footer: React.FC = () => {
         border-t 
         border-solid 
         border-gray-200">
-        <a
-          className="
+      <a
+        className="
           flex 
           flex-1 
           justify-center 
           items-center"
-          href="
+        href="
           https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className="h-4 ml-2">
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    )
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{" "}
+        <span className="h-4 ml-2">
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
+      </a>
+    </footer>
+  )
 }
 export default Footer
