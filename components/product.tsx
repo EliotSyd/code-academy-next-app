e92446b025c1493f461cbf5f37b24acc88d7c052
EliotSyd <eliot.sydenham@gmail.com>
2021-09-17 20:11:27 +0100

    Changed

--- components/product.tsx
+++ components/product.tsx
@@ -9,20 +9,6 @@ const Product: React.FC<IProduct> = ({ product_id, product_name, selling_price, 
   return (
     <Link href={`/${product_id}`}>
       <a className="m-3 w-96 text-left no-underline border border-solid border-gray-200 rounded-lg hover:shadow-md focus:shadow-md focus:outline-none">
-        <div
-          id="image"
-          className={classNames(
-            `h-64 rounded-t-lg flex items-center justify-center`,
-            {
-              "bg-blue-100": image === 0,
-              "bg-pink-100": image === 1,
-              "bg-purple-100": image === 2,
-              "bg-green-100": image === 3,
-            }
-          )}
-        >
-          <span className="material-icons text-6xl text-white">image</span>
-        </div>
         <div className="p-5">
           <h2 className="m0 mb-4 text-2xl font-medium">{product_name}</h2>
           <p className="m-0 text-xl">Category: {product_id}</p>
