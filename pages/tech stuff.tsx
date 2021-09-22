c044fcc4b56b93cd3de266c345fd1cb6ace0ecc2
EliotSyd <eliot.sydenham@gmail.com>
2021-09-22 14:21:22 +0100

    Change

---
+++ pages/tech stuff.tsx
@@ -0,0 +1,21 @@
+import Product from "../components/product";
+import { IProduct } from "../models/product";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+    const [products, setProducts] = useState<IProduct[]>([]);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const res = await (await fetch(`api/products/category/tech stuff`)).json();
+            setProducts(res);
+        };
+        fetchProducts();
+    }, []);
+    return (
+        <div className="mt-10 flex flex-wrap flex-col sm:flex-row w-full justify-center items-center">
+            {products.map((p) => (
+                <Product key={p.product_id} {...p} />
+        ))}
+        </div>
+    )
+}
